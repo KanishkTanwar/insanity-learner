7b2e39ccfd0027e9d7dddc9afee0388caafc0268
kanishktanwar <kanishk.tanwar7@gmail.com>
2021-08-10 14:08:34 +0530

    login functionality add

--- register/urls.py
+++ register/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from register.views import signin, signout
+from register.views import signin, signout, signup
 
 app_name = "register"
 
 urlpatterns = [
     path('signout', signout, name="signout"),
     path('signin', signin, name="signin"),
+    path('signup', signup, name="signup"),
 ]
--- register/views.py
+++ register/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import CreateUserForm
+from django.contrib import messages
 
 # Create your views here.
 
 
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home:index')
+    else:
+        form = CreateUserForm()
+        context = {'form': form}
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+
+                return redirect('register:signin')
+
+        return render(request, 'register/signin.html', context)
+
+
 def signin(request):
-    return render(request, 'register/signin.html')
+    if request.user.is_authenticated:
+        return redirect('home:index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home:index')
+            else:
+                messages.warning(request, 'Username OR password is incorrect')
+
+        return render(request, 'register/signin.html')
 
 
 def signout(request):
-    return render(request, 'register/signin.html')
+    logout(request)
+    return redirect('home:index')
